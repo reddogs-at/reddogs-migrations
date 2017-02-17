c6971bbbfcda997b4c250d425b63af1b4d8e8279
jolicht <johannes@lichtenwallner.at>
2017-02-17 11:53:37 +0100

    added command class skeletons

---
+++ src/Command/StatusCommand.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Reddogs (https://github.com/reddogs-at)
+ *
+ * @see https://github.com/reddogs-at/reddogs-migrations for the canonical source repository
+ * @license https://github.com/reddogs-at/reddogs-migrations/blob/master/LICENSE MIT License
+ */
+declare(strict_types = 1);
+namespace Reddogs\Migrations\Command;
+
+class StatusCommand
+{
+
+}
